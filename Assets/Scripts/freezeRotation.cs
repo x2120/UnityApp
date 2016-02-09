257d7e030bb1eb02dd1ffd84c950c70c05830173
x2120 <tinvou@gmail.com>
2016-02-09 00:48:08 -0500

    0.008 Ship rotation Fix
    Ship no longer tumbles

---
+++ Assets/Scripts/freezeRotation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class freezeRotation : MonoBehaviour
+{
+    public RigidbodyConstraints constraints;
+
+    void Start()
+    {
+        constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
+    }
+}
+
+
+
+
+
