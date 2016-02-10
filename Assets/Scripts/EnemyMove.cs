174d6f98990e95c7e2a3b3c5a79f39e33ff5fe38
x2120 <tinvou@gmail.com>
2016-02-10 02:41:29 -0500

    0.011 - Waves
    Enemies now spawn in waves!

---
+++ Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMove : MonoBehaviour
+{
+    public float speed;
+
+    void Start()
+    {
+        Vector3 movement = new Vector3(1.0f, 0.0f, 0.0f);
+        GetComponent<Rigidbody>().velocity = movement * speed;
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject hazard;
+    public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+
+    void Start()
+    {
+        StartCoroutine(SpawnWaves());
+    }
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                Vector3 spawnPosition = new Vector3(spawnValues.x,  spawnValues.y, Random.Range(-spawnValues.z, spawnValues.z));
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+        }
+    }
+}
