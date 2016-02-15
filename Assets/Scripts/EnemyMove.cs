25527406aaf89fb1035c2da361a43b7f08ee01b1
Justin <tinvou@gmail.com>
2016-02-15 01:41:50 -0500

    0.014 -Enemy movement tests
    Testing enemy movement patterns

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -4,10 +4,20 @@ using System.Collections;
 public class EnemyMove : MonoBehaviour
 {
     public float speed;
+    private Vector3 pos;
 
     void Start()
     {
+        pos = transform.position;
         Vector3 movement = new Vector3(1.0f, 0.0f, 0.0f);
         GetComponent<Rigidbody>().velocity = movement * speed;
+    }
+
+    void Update()
+    {
+        //for ship movement using math formauls 
+        //here we need to update the z of the bullet so we can generate patterns
+        pos += transform.right * Time.deltaTime * speed;
+        transform.position = pos * Mathf.Sin(Time.time);
     }
 }
